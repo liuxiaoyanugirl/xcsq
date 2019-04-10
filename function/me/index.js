929c78a8384149842c376f38141c81b28a01c635
student <student@hubu.edu.cn>
2019-04-10 11:25:33 +0800

    inti

--- function/me/index.js
+++ function/me/index.js
@@ -6,6 +6,14 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
+  return await db.collection('order').add({
+    data: {
+      user: wxContext.OPENID,
+      action: wxContext.OPENID + '新增订单'
+    }
+  })
+}
+
 
   return {
     event,
