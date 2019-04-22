e132ec833b8bce491248d40523ce5cbd8ba11968
student <student@hubu.edu.cn>
2019-04-22 17:13:01 +0800

    inti

---
+++ src/pages/change/index.vue
@@ -0,0 +1,123 @@
+<template>
+  <div><i-tabs :current="current_scroll" scroll @change="handleChangeScroll">
+    <i-tab key="tab1" title="尊贵皮肤"></i-tab>
+    <i-tab key="tab2" title="精美头饰"></i-tab>
+    <i-tab key="tab3" title="动态卡片"></i-tab>
+</i-tabs>
+<image style = "width : 360px; height : 180px;" class="slide-image" src="cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/meitu.jpg" mode="scaleToFill"/> 
+      <view class="top-padding">
+      <i-card title="精选"  thumb="http://www.lnfda.com/uploads/allimg/150731/1-150I11506424E.jpg">
+      </i-card>
+      <view class="top-padding"></view>
+      <i-card title="卡片标题" i-class="top-padding"  thumb="http://img2.imgtn.bdimg.com/it/u=3359603132,2052509191&fm=214&gp=0.jpg">
+      </i-card>
+    </view>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      current_scroll: 'tab1',
+      motto: 'Hello miniprograme',
+      userInfo: {
+        nickName: 'mpvue',
+        avatarUrl: 'http://mpvue.com/assets/logo.png'
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    handleChange ({ detail }) {
+    this.setData({
+      current: detail.key
+    }); 
+    },
+    handleChangeScroll (event) {
+    this.current_scroll = event.mp.detail.key
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+div >>> .no-border {
+  border-width: 0pt;
+}
+.top-padding {
+  padding-top: 50rpx;
+}
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,17 +1,19 @@
 <template>
-  <div><i-tabs :current="current_scroll" scroll @change="handleChangeScroll">
-    <i-tab key="tab1" title="尊贵皮肤"></i-tab>
-    <i-tab key="tab2" title="精美头饰"></i-tab>
-    <i-tab key="tab3" title="动态卡片"></i-tab>
-</i-tabs>
-<image style = "width : 360px; height : 180px;" class="slide-image" src="cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/meitu.jpg" mode="scaleToFill"/> 
-      <view class="top-padding">
-      <i-card title="精选"  thumb="http://www.lnfda.com/uploads/allimg/150731/1-150I11506424E.jpg">
-      </i-card>
-      <view class="top-padding"></view>
-      <i-card title="卡片标题" i-class="top-padding"  thumb="http://img2.imgtn.bdimg.com/it/u=3359603132,2052509191&fm=214&gp=0.jpg">
-      </i-card>
-    </view>
+  <div style="width:100%; height:100%">
+    <map
+  id="map"
+  longitude="113.324520"
+  latitude="23.099994"
+  scale="14"
+  controls="{{controls}}"
+  bindcontroltap="controltap"
+  markers="{{markers}}"
+  bindmarkertap="markertap"
+  polyline="{{polyline}}"
+  bindregionchange="regionchange"
+  show-location
+  style="width: 100%; height: 300px;"
+></map>
   </div>
 </template>
 
@@ -21,103 +23,42 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      current_scroll: 'tab1',
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
+    markers: [{
+      iconPath: '/static/grid/1.png',
+      id: 0,
+      latitude: 30.571331,
+      longitude: 114.347997,
+      width: 50,
+      height: 50
+    }],
+}
   },
+
 
   components: {
     card
   },
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
+    regionchange(event) {
+        console.log(event)
+    },
+          markertap(event) {
+        console.log(event)
+      },
+
+    controltap(event) {
+        console.log(event)
       }
-    },
-    handleChange ({ detail }) {
-    this.setData({
-      current: detail.key
-    }); 
-    },
-    handleChangeScroll (event) {
-    this.current_scroll = event.mp.detail.key
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
-    }
   },
 
   created () {
     // let app = getApp()
-  }
-}
+  },
+    }
+  
 </script>
 
-<style scoped>
-div >>> .no-border {
-  border-width: 0pt;
-}
-.top-padding {
-  padding-top: 50rpx;
-}
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
+<style >
+body,html{height:100%}
 </style>
