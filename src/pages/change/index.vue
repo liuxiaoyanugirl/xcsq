97abcb9dd729bae7578796c249693262275b5ef9
me <2643972508@qq.com>
2019-05-27 20:49:42 +0800

    增加Home页面

--- src/pages/change/index.vue
+++ src/pages/change/index.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
-  <Tabs>
-        <Tab-pane label="macOS" icon="social-apple">标签一的内容</Tab-pane>
-        <Tab-pane label="Windows" icon="social-windows">标签二的内容</Tab-pane>
-        <Tab-pane label="Linux" icon="social-tux">标签三的内容</Tab-pane>
-    </Tabs>
-      <view class="top-padding">
-      <i-card title="精选"  thumb="http://www.lnfda.com/uploads/allimg/150731/1-150I11506424E.jpg">
-      </i-card>
-      <view class="top-padding"></view>
-      <i-card title="卡片标题" i-class="top-padding"  thumb="http://img2.imgtn.bdimg.com/it/u=3359603132,2052509191&fm=214&gp=0.jpg">
-      </i-card>
-    </view>
+<i-tabs :current="current" @change="handleChange">
+    <i-tab key="tab1" title="学院日常"></i-tab>
+    <i-tab key="tab2" title="面试轻熟"></i-tab>
+    <i-tab key="tab3" title="出游度假"></i-tab>
+    <i-tab key="tab4" title="运动朋克"></i-tab>
+</i-tabs>
+  <view v-if="show1">  
+    <i-card style="padding: 15px;" v-for="item in top1" :key="item">
+    <view slot="content"><image style="width:100%;height:80pt;" :src="item.image" /></view>
+    <view slot="footer">{{item.introduce}}</view>    
+    </i-card>
+  </view>
+  <view v-if="show2">
+     <i-card style="padding: 15px;" v-for="item in top2" :key="item">
+     <view slot="content"><image style="width:100%;height:80pt;" :src="item.image" /></view>
+     <view slot="footer">{{item.introduce}}</view>    
+     </i-card>
+  </view>
+  <view v-if="show3">
+     <i-card style="padding: 15px;" v-for="item in top3" :key="item">
+     <view slot="content"><image style="width:100%;height:80pt;" :src="item.image" /></view>
+     <view slot="footer">{{item.introduce}}</view>    
+     </i-card>
+  </view>
+  <view v-if="show4">
+     <i-card style="padding: 15px;" v-for="item in top4" :key="item">
+     <view slot="content"><image style="width:100%;height:80pt;" :src="item.image" /></view>
+     <view slot="footer">{{item.introduce}}</view>    
+     </i-card>
+  </view>
   </div>
 </template>
 
@@ -21,103 +39,74 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      current_scroll: 'tab1',
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
-    }
-  },
-
+        current:'tab1',
+        show1: true,
+        show2: false,
+        show3: false,
+        show4: false,
+        top1: [
+         {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/dailyboy1.jpg",introduce:"“休闲舒适的日常穿搭，元气满满"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/daily1.jpg",introduce:"休闲舒适的日常穿搭，元气满满"},  
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/daily2.jpg",introduce:"“休闲舒适的日常穿搭，元气满满"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/daily3.jpg",introduce:"休闲舒适的日常穿搭，元气满满"},            
+            ],
+        top2: [
+         {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/1.jpeg",introduce:"“成熟稳重的服装搭配，让你自信面对生活挑战"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/2.jpeg",introduce:"成熟稳重的服装搭配，让你自信面对生活挑战"},  
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/3.jpeg",introduce:"“成熟稳重的服装搭配，让你自信面对生活挑战"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/5.jpeg",introduce:"成熟稳重的服装搭配，让你自信面对生活挑战"},            
+            ],
+        top3: [
+         {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/vac1.jpg",introduce:"“度假就要挑眼，拍照张扬才好看"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/vac 2.jpg",introduce:"度假就要挑眼，拍照张扬才好看"},  
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/vac 3.jpg",introduce:"“度假就要挑眼，拍照张扬才好看"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/vac4.jpg",introduce:"度假就要挑眼，拍照张扬才好看"},            
+            ],
+        top4: [
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/cool1.jpg",introduce:"“度假就要挑眼，拍照张扬才好看"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/cool2.jpg",introduce:"度假就要挑眼，拍照张扬才好看"},  
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/cool3.jpg",introduce:"“度假就要挑眼，拍照张扬才好看"},
+          {image:"cloud://xcsq-0f8ce5.7863-xcsq-0f8ce5/xcsq/cool4.jpg",introduce:"度假就要挑眼，拍照张扬才好看"},            
+            ]
+      }     
+    },
   components: {
     card
   },
-
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    handleChange ({ detail }) {
-    this.setData({
-      current: detail.key
-    }); 
-    },
-    handleChangeScroll (event) {
-    this.current_scroll = event.mp.detail.key
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
+    handleChange ( detail ) {
+      console.log(detail)
+        this.current = detail.mp.detail.key
+        if (this.current === 'tab1'){
+          this.show1 = true;
+          this.show2 = false;
+          this.show3 = false;
+          this.show4 = false;
+        }else if(this.current === 'tab2'){
+          this.show1 = false;
+          this.show2 = true;
+          this.show3 = false;
+          this.show4 = false;
+        }else if(this.current ==='tab3'){
+          this.show1 = false;
+          this.show2 = false;
+          this.show3 = true;
+          this.show4 = false;
+        }else{
+          this.show1 = false;
+          this.show2 = false;
+          this.show3 = false;
+          this.show4 = true;
+        }
     }
+
   },
 
   created () {
-    // let app = getApp()
+
   }
 }
 </script>
 
 <style scoped>
-div >>> .no-border {
-  border-width: 0pt;
-}
-.top-padding {
-  padding-top: 50rpx;
-}
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
 </style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,19 @@
+<template> 
+    <div class="note" :style ="note"></div>
+</template>
+
+<script>
+export default {
+name: 'productdetailspage',
+data() {
+ return {
+  note: {
+   backgroundImage: "url(" + require("../../iamges/1.jpg") + ")",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "25px auto",
+  marginTop: "5px",
+  },
+ }
+},
+}
+</script>
